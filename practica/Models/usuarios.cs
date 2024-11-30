3fe1f9787c4d40639a11be2dac09b580ec248e51
BRYAMS G. SOLORZANO <solorzanobryams@outlook.com>
2024-11-30 09:15:05 -0500

    Se sube rama principal

---
+++ practica/Models/usuarios.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace practica.Models
+{
+    public class Usuarios
+    {
+
+        [Key]// llave//
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]//identity aunto incremental//
+        public int Id {  get; set; }
+        [Required(AllowEmptyStrings =false)] //requerido sin espacios//
+        [StringLength(50)] //numero de caracteres//
+        public string Username { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+
+    }
+}
---
+++ practica/context/webaplicationcontext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity; /*agregar*/
+using System.Linq;
+using System.Web;
+using practica.Models; //agregar//
+
+namespace practica.context
+{
+    public class webaplicationcontext : DbContext
+    {
+        public webaplicationcontext() : base("name=Prueba")
+        {
+        }
+
+        public DbSet<Usuarios> Usuarios { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) //modificacion de metodos//
+        {
+            modelBuilder.Entity<Usuarios>()
+             .ToTable("Usuarios")
+             .HasKey(p => p.Id);
+
+            modelBuilder.Entity<Usuarios>()  //llamamos tabla y ponesmos atributos//
+                .Property(p => p.Id)
+                .IsRequired()
+                .HasColumnName("Id");
+
+            modelBuilder.Entity<Usuarios>()
+                .Property(p => p.Username)
+                .IsRequired()
+                .HasColumnName("Username")
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Usuarios>()
+                .Property(p => p.Password)
+                .IsRequired()
+                .HasColumnName("Password")
+                .HasMaxLength(50);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+}
